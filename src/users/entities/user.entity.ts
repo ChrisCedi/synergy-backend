24f8154a40f788279ee339b03bb710ad1c3cd277
ChrisCedi <christian.cedillo.palacios@outlook.com>
2025-06-07 08:44:47 -0600

    password nullable test

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -21,7 +21,7 @@ export class User {
   @Column({ type: 'varchar', length: 100, unique: true })
   email: string;
 
-  @Column({ type: 'varchar', length: 100 })
+  @Column({ type: 'varchar', length: 100, nullable: true })
   password: string;
 
   @Column({ type: 'varchar', length: 50 })
